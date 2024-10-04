2c7f8fa8c7883404a941ee96e065d395e2d74970
tiagorahal <rahal.aires@gmail.com>
2024-10-04 12:45:23 -0300

    Adjust sorting details

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -2,16 +2,20 @@ class MonstersController < ApplicationController
   def index
     page = params[:page] || 1
     sort_order = params[:sort] || 'asc'
-
-    response = Monsters::ListService.call(page, sort_order)
+    sort_by = params[:sort_by] || 'name'
+    
+    Rails.logger.info "Controller params - Sort by: #{sort_by}, Sort order: #{sort_order}"
+    
+    response = Monsters::ListService.call(page, sort_order, sort_by)
     @monsters = response[:monsters]
     @pagination = response[:pagination]
     @current_sort_order = sort_order
+    @current_sort_by = sort_by
   end
 
   def show
     monster_index = params[:id]
-  
+
     @monster = Monsters::ShowService.call(monster_index)
     if @monster.nil?
       flash[:alert] = "Unable to fetch monster details at this time. Please try again later."
--- app/graphql/monsters/list_service.rb
+++ app/graphql/monsters/list_service.rb
@@ -1,22 +1,29 @@
 module Monsters
   class ListService
-    def self.call(page, sort_order = 'asc')
+    def self.call(page, sort_order = 'asc', sort_by = 'name')
       monster_list = DndApiService.get_monster_list["results"]
 
-      sorted_monster_list = monster_list.sort_by { |monster| monster['name'] }
+      sorted_monster_list = monster_list.sort_by { |monster| monster[sort_by] }
+
       sorted_monster_list.reverse! if sort_order == 'desc'
 
       paginated_monster_list = Kaminari.paginate_array(sorted_monster_list).page(page).per(15)
-      
+
       monster_details = paginated_monster_list.map do |monster|
         details = DndApiService.get_monster_details(monster["index"])
-        {
-          name: details["name"],
-          image: "https://www.dnd5eapi.co#{details['image']}",
-          type: details["type"] || "Unknown type",
-          index: details["index"]
-        }
-      end
+
+        if details.nil?
+          Rails.logger.error("Failed to fetch details for monster: #{monster['index']}")
+          next
+        else
+          {
+            name: details["name"],
+            image: details['image'].present? ? "https://www.dnd5eapi.co#{details['image']}" : nil,
+            type: details["type"] || "Unknown type",
+            index: details["index"]
+          }
+        end
+      end.compact
 
       { monsters: monster_details, pagination: paginated_monster_list }
     end
